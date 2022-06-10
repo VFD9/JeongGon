825f62440a9d62100df9015021be609b56c376df
KimJeongGon <niceman1994@naver.com>
2022-06-10 11:42:10 +0900

    2022.06.10 STLVector v0.2

--- STLVector/STLVector/main.cpp
+++ STLVector/STLVector/main.cpp
@@ -1,4 +1,4 @@
-// STLVector v0.1
+// STLVector v0.2
 #include <iostream>
 #include <vector>
 
@@ -6,13 +6,37 @@ using namespace std;
 
 int main(void)
 {
+	int MAX = 50;
+
 	vector<int> vecNumbers;
 
-	for(int i = 0; i < 10; ++i)
+	for (int i = 0; i < 10; ++i)
+	{
 		vecNumbers.push_back(10 * i);
+		//cout << "apacity : " <<  vecNumbers.capacity() << endl;
+		//cout << "size : " << vecNumbers.size() << endl << endl;
+	}
 
-	for (vector<int>::iterator iter = vecNumbers.begin(); iter != vecNumbers.end(); ++iter)
+	// 가장 앞에 있는 원소를 반환
+	vecNumbers.front();
+
+	// 가장 뒤에 있는 원소를 반환
+	vecNumbers.back();
+
+	// 시작점을 가리키는 반복자
+	vecNumbers.begin();
+
+	// 종료 지점을 가리키는 반복자
+	vecNumbers.end();
+
+	/*for (vector<int>::iterator iter = vecNumbers.begin(); iter != vecNumbers.end(); ++iter)
 		cout << (*iter) << endl;
+
+	for (auto iter = vecNumbers.begin(); iter != vecNumbers.end(); ++iter)
+		cout << (*iter) << endl;
+
+	for (int i = 0; i < vecNumbers.size(); ++i)
+		cout << vecNumbers[i] << endl;*/
 
 	/*// 직접 접근이 가능한 형태인 배열
 	int Array[16] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
@@ -26,10 +50,19 @@ int main(void)
 	for (int i = 0; i < 16; ++i)
 		cout << *(iterator + i) << endl;*/
 
+	/*for (auto iter = vecNumbers.begin(); iter != vecNumbers.end();)
+	{
+		if ((*iter) > MAX)
+			iter = vecNumbers.erase(iter);
+		else
+			++iter;
+	}
+
+	for (auto iter = vecNumbers.begin(); iter != vecNumbers.end(); ++iter)
+		cout << (*iter) << endl;*/
+
 	return 0;
 }
-
-
 
 // 지역 변수는 stack에 있고 대입되는 값은 heap에 있다.
 // 원본 데이터에 값을 반영하고 싶으면 포인터(Call by Reference)를 사용한다.
